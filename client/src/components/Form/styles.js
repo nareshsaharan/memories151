fe6332736924f45433a06de8468bd6e835301a17
nareshsaharan <nareshsaharan2016@gmail.com>
2020-12-28 15:29:31 +0530

    first commit

---
+++ client/src/components/Form/styles.js
@@ -0,0 +1,27 @@
+
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+    },
+  },
+  paper: {
+    padding: theme.spacing(2),
+    boxShadow: 'rgba(53, 53, 53, 0.2) 0px 1px 4px 0px',
+    borderRadius: '8px',
+  },
+  form: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  fileInput: {
+    width: '97%',
+    margin: '10px 0',
+  },
+  buttonSubmit: {
+    marginBottom: 10,
+  },
+}));
---
+++ client/src/components/Posts/Post/styles.js
@@ -0,0 +1,54 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles({
+  media: {
+    height: 0,
+    paddingTop: '56.25%',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    backgroundBlendMode: 'darken',
+  },
+  border: {
+    border: 'solid',
+  },
+  fullHeightCard: {
+    height: '100%',
+  },
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    borderRadius: '15px',
+    height: '100%',
+    position: 'relative',
+    boxShadow: 'rgba(53, 53, 53, 0.2) 0px 1px 4px 0px',
+    borderRadius: '8px',
+  },
+  overlay: {
+    position: 'absolute',
+    top: '20px',
+    left: '20px',
+    color: 'white',
+  },
+  overlay2: {
+    position: 'absolute',
+    top: '20px',
+    right: '20px',
+    color: 'white',
+  },
+  grid: {
+    display: 'flex',
+  },
+  details: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    margin: '20px',
+  },
+  title: {
+    padding: '0 16px',
+  },
+  cardActions: {
+    padding: '0 16px 8px 16px',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+});

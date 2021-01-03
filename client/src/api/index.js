263d0abc55596419dd036d3ba8c8714f1e5cdfb7
nareshsaharan <nareshsaharan2016@gmail.com>
2021-01-03 13:28:13 +0530

    first commit
    Signed-off-by: nareshsaharan <nareshsaharan2016@gmail.com>

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:4000/posts';
+const url = 'https://memories-api151.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
